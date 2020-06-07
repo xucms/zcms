740bfdc050da45353f2b0a3a929b306b2e564604
liucms <65150550+liucms@users.noreply.github.com>
2020-06-07 22:36:16 +0800

    zcms
    zcms

--- app/views/index.php
+++ app/views/index.php
@@ -3,10 +3,13 @@
 <head>
 <meta charset="utf-8">
 <title>{$title}</title>
-<script src="/public/creator/jquery.min.js"></script>
-<script src="/public/creator/jquery.cookie.js"></script>
+<script src="/public/creator/jquery/jquery.min.js"></script>
+<script src="/public/creator/jquery/jquery.cookie.js"></script>
+<link rel="stylesheet" type="text/css" href="/public/creator/css/common.min.css">
 </head>
 <body>
+
+<h1>{$title}</h1>
 
 </body>
 </html>
